1f643f4be441d9477381e07ca7868e464aafdb91
Elise Jones <02elise.jones02@gmail.com>
2023-09-12 10:50:38 -0600

    Add routes and component files

---
+++ news-challenge/src/Components/App/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Homepage from '../Homepage/Homepage';
+import DetailsPage from '../DetailsPage/DetailsPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/details' element={<DetailsPage />} />
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
